0644ca9f49a6f567309d0360357751ea6cfc5d5b
Hampus Rönström <h_ronstrom@hotmail.com>
2019-11-06 00:55:05 +0100

    Cleanup

--- src/containers/ContractListView.js
+++ src/containers/ContractListView.js
@@ -1,12 +1,8 @@
 import { connect } from 'react-redux';
 import ContractList from '../components/ContractList';
 
-const getAllContracts = (contracts) => {
-    return contracts;
-}
-
 const mapStateToProps = state => ({
-    contracts: getAllContracts(state.contracts)
+    contracts: state.contracts
 });
 
 export default connect(mapStateToProps)(ContractList);
