91b9e31aab2baa0d747960947b67ab4f06b0399a
Hampus Rönström <h_ronstrom@hotmail.com>
2019-11-05 23:28:50 +0100

    Adds routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import CustomerListView from './containers/CustomerListView';
 import ContractListView from './containers/ContractListView';
 
 function App() {
   return (
-    <div className="App">
-      <CustomerListView/>
-      <ContractListView/>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/contracts">
+            <ContractListView />;
+          </Route>
+          <Route path="/customers">
+            <CustomerListView />;
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
+
+function Home() {
+  return (
+    <div class="center-screen">
+      <h1>The Contract Automation Platform</h1>
+      <nav>
+        <Link to="/contracts">Contracts</Link>
+        <Link to="/customers">Customers</Link>
+      </nav>
+    </div>
+  );
+}
 export default App;
